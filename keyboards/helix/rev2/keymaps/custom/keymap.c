47058ed04bde3dfe9f57d7964fbd2aabe7377784
Kenya Kawatsu <3982ne@gmail.com>
2019-09-02 18:34:31 +0900

    fix arrow

--- keyboards/helix/rev2/keymaps/custom/keymap.c
+++ keyboards/helix/rev2/keymaps/custom/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Alt  | Alt  |   N  |   M  |   ,  |   .  |   /  |   `  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Lower |Caps  |Adjust| Alt  |Bspace| Cmd  |Space |Space | Cmd  |Enter |   [  |   ]  |  \   |Raise |
+   * |Lower |Caps  |Adjust| Alt  | Cmd  |Space |Bspace|Enter |Space | Cmd  |   [  |   ]  |  \   |Raise |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_RCMD,  KC_RCMD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV, \
-      LOWER,   KC_CAPS, ADJUST,  KC_EJCT ,KC_LALT,KC_SPC,  KC_BSPC,  KC_ENT,  KC_SPC,  KC_LALT, KC_LBRC, KC_RBRC, KC_BSLS, RAISE \
+      LOWER,   KC_CAPS, ADJUST,  KC_EJCT ,KC_LALT, KC_SPC, KC_BSPC,  KC_ENT,  KC_SPC,  KC_LALT, KC_LBRC, KC_RBRC, KC_BSLS, RAISE \
       ),
   /* なぜかmacOSでAltとMCDが逆に認識される
    */
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
       KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
       ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
@@ -146,21 +146,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |  Up  |      |      |      |             |      |      |      |      |      | Del  |
+   * |      |      |  Up  |      |      |      |             |      |      |      |      |  Up  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      | Left | Down |Right |      |      |             | Left | Down |  Up  |Right |      |      |
+   * |      | Left | Down |Right |      |      |             |      |      |      | Left |Right |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |      |  Up  |      |      |
+   * |      |      |      |      |      |      |      |      |      |      |      |      | Down |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------|------|
-   * |      |      |      |      |      |      |      |      |      |      | Left | Down |Right |      |
+   * |      |      |      |      |      |      |Bspace|Enter |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-      _______, _______, KC_UP,   _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______ \
+      _______, _______, KC_UP,   _______, _______, _______,                   _______, _______, _______, _______, KC_UP,   _______, \
+      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   _______, _______, _______, KC_LEFT, KC_RGHT, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DOWN, _______, \
+      _______, _______, _______, _______, _______, _______, KC_SPC,  KC_ENT,  _______, _______, _______, _______, _______, _______ \
       ),
 
   /* Adjust (Lower + Raise)
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      | Reset|RGBRST|      |      |      |             |      |      |      |      |      |  Del |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |Aud on|Audoff| Mac  |             | Win  |Qwerty|Colemk|Dvorak|      |      |
+   * |      |      |      |Aud on|Audoff| Mac  |             |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      |RGB ON| HUE+ | SAT+ | VAL+ |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] =  LAYOUT( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
       _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL, \
-      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
+      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   _______, _______, _______, _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD \
       )
